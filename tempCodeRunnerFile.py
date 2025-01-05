5ecfacab47456027c6a5798a62a699bb2a4c4d7c
Yuvraj <yuvrajkharar@gmail.com>
2025-01-05 18:39:00 +0530

    Fixed line endings and initialized submodules

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+from playsound import playsound
+from threading import Thread
